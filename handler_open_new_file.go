f5af13d399c94796cf029c58c42e3b6ed9bd2c64
Alexander Hansen <ahansen1335@gmail.com>
2025-03-25 22:18:40 +0100

    open new index.md file for editing and save the edits to the current working directory (for now) with an automated url created

--- handler_open_new_file.go
+++ handler_open_new_file.go
@@ -4,31 +4,60 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"regexp"
+	"strings"
 	"time"
+
+	"gopkg.in/yaml.v2"
 )
+
+type MetaData struct {
+	Title      string    `yaml:"title"`
+	Date       time.Time `yaml:"date"`
+	URL        string    `yaml:"url"`
+	Categories []string  `yaml:"categories"`
+	Tags       []string  `yaml:"tags"`
+	Content    string    `yaml:"-"`
+}
 
 func handlerNewFile(s *state, cmd command) error {
 	editor := s.config.Editor
-	tmpFile, err := os.CreateTemp("", "index.md")
+	tmpFile, err := os.CreateTemp("", "index*.md")
 	if err != nil {
 		return err
 	}
 	defer os.Remove(tmpFile.Name())
 
 	templateText := fmt.Sprintf(`---
-title: 
-url:
+title: 'Testing File'
 date: %v
+url: 
 categories: 
+    - example 1
+    - example 2
 tags:
-  - 
-  - 
+    - example 3
+    - example 4
 ---
-`, time.Now())
+`, time.Now().Format(time.RFC3339))
 
-	if err := os.WriteFile(tmpFile.Name(), []byte(templateText), 0644); err != nil {
+	file, err := os.Create(tmpFile.Name())
+	if err != nil {
 		return err
 	}
+
+	_, err = file.Write([]byte(templateText))
+	if err != nil {
+		return err
+	}
+
+	err = file.Sync()
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	file.Close()
 
 	open := exec.Command(editor, tmpFile.Name())
 	open.Stdin = os.Stdin
@@ -46,9 +75,63 @@ tags:
 	}
 
 	finalFile := "index.md"
-	if err := os.WriteFile(finalFile, content, 0644); err != nil {
+
+	file, err = os.Create(finalFile)
+	if err != nil {
 		return err
 	}
 
+	_, err = file.Write(content)
+	if err != nil {
+		file.Close()
+		return nil
+	}
+
+	err = file.Sync()
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	file.Close()
+
+	savedContent, err := os.ReadFile(finalFile)
+
+	data, err := parseContent(savedContent)
+
+	if err := os.WriteFile(finalFile, data, 0644); err != nil {
+		return nil
+	}
+
 	return nil
+}
+
+func parseContent(content []byte) ([]byte, error) {
+	contentStr := string(content)
+
+	re := regexp.MustCompile(`(?s)^---\n(.*?)\n---\n(.*)`)
+	matches := re.FindStringSubmatch(contentStr)
+
+	var metaData MetaData
+
+	if len(matches) == 3 {
+		err := yaml.Unmarshal([]byte(matches[1]), &metaData)
+		if err != nil {
+			fmt.Println("ERROR: Failed to parse YAML:", err)
+			return []byte{}, err
+		}
+		metaData.Content = matches[2]
+	} else {
+		metaData.Content = contentStr
+	}
+
+	metaData.URL = fmt.Sprintf("/%v", strings.ToLower(strings.Join(strings.Split(metaData.Title, " "), "-")))
+
+	data, err := yaml.Marshal(metaData)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	formattedContent := fmt.Sprintf("---\n%s---\n%s", data, metaData.Content)
+	return []byte(formattedContent), nil
 }
